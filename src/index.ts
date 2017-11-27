0ce1c3605122d20b58e5a30fa673917eba6f70d1
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-11-27 19:12:00 +0100

    added mp3 audio file, bunch of other changes :P

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
 import webAudioTouchUnlock from 'web-audio-touch-unlock'
 
-let context = ((<any>window).AudioContext || (<any>window).webkitAudioContext)();
+let loaded = false;
+let locked = true;
 
-webAudioTouchUnlock(context);
+let context = new ((<any>window).AudioContext || (<any>window).webkitAudioContext)();
+
+webAudioTouchUnlock(context).then((ulocked: boolean)=>{
+    console.log('All good!');
+}, (string: string)=>{
+    console.log(string);
+});
+
+// You're fine, try it on a mobile device.
+// Tap anywhere to unlock... -> There you go!
+
+// ERROR!
+// [error message]
+/*Seems like this approach can not be used with current ' +
+'implementation of AudioContext. We\'re sorry about that, however you can open an issue here: ' +
+'https://github.com/pavle-goloskokovic/web-audio-touch-unlock/issues and we\'ll try to sort it out.');*/
+
+// Loading audio... -> Decoding audio... -> Waiting for touch unlock... -> Playing audio!
 
 let request = new XMLHttpRequest();
-request.open('GET', 'viper.ogg', true);
+request.open('GET', 'assets/audio/3667624464.mp3', true);
 request.responseType = 'arraybuffer';
 request.onload = function()
 {
-    context.decodeAudioData(request.response,
-        (buffer: AudioBuffer) =>
+    context.decodeAudioData(request.response, (buffer: AudioBuffer) =>
         {
             let source = context.createBufferSource();
             source.buffer = buffer;
