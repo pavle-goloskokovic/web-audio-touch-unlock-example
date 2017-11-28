c5eed497fdd3cd101d8826bc302764aa40c245b6
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2017-11-28 16:54:40 +0100

    added index.html, updated index.ts with nice message printing, did a new build

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
-import webAudioTouchUnlock from 'web-audio-touch-unlock'
+import webAudioTouchUnlock from 'web-audio-touch-unlock';
 
 let loaded = false;
 let locked = true;
+let userUnlocked = false;
 
 let context = new ((<any>window).AudioContext || (<any>window).webkitAudioContext)();
 
-webAudioTouchUnlock(context).then((ulocked: boolean)=>{
-    console.log('All good!');
-}, (string: string)=>{
-    console.log(string);
-});
+webAudioTouchUnlock(context).then((unlocked: boolean) => {
 
-// You're fine, try it on a mobile device.
-// Tap anywhere to unlock... -> There you go!
+        locked = false;
+        userUnlocked = unlocked;
+
+        if(userUnlocked)
+        {
+            if(loaded)
+            {
+                setMessage('message', 'there');
+
+                setMessage('status', 'playing');
+            }
+            else
+            {
+                setMessage('message', 'abit');
+            }
+        }
+        else
+        {
+            setMessage('message', 'fine');
+        }
+    },
+    (reason: any) => {
+        // TODO print error
+        console.log(reason);
+    }
+);
 
 // ERROR!
 // [error message]
@@ -20,24 +41,76 @@ webAudioTouchUnlock(context).then((ulocked: boolean)=>{
 'implementation of AudioContext. We\'re sorry about that, however you can open an issue here: ' +
 'https://github.com/pavle-goloskokovic/web-audio-touch-unlock/issues and we\'ll try to sort it out.');*/
 
-// Loading audio... -> Decoding audio... -> Waiting for touch unlock... -> Playing audio!
-
 let request = new XMLHttpRequest();
 request.open('GET', 'assets/audio/3667624464.mp3', true);
 request.responseType = 'arraybuffer';
-request.onload = function()
+request.onload = () =>
 {
-    context.decodeAudioData(request.response, (buffer: AudioBuffer) =>
-        {
+    setMessage('status', 'decoding');
+
+    context.decodeAudioData(request.response, (buffer: AudioBuffer) => {
+
             let source = context.createBufferSource();
             source.buffer = buffer;
             source.connect(context.destination);
             source.start();
+
+            loaded = true;
+
+            if (locked)
+            {
+                setMessage('status', 'waiting');
+            }
+            else
+            {
+                if(userUnlocked)
+                {
+                    setMessage('message', 'there');
+                }
+
+                setMessage('status', 'playing');
+            }
         },
-        (e: ErrorEvent) =>
-        {
+        (e: ErrorEvent) => {
+            // TODO print error
             console.log("Error with decoding audio data" + e);
         }
     );
 };
 request.send();
+
+let setMessage = (id: string, key: string) =>
+{
+    let messages: { [key: string]: string } = {
+        tap:      `         Tap anywhere to unlock...`,
+        loading:  `             Loading audio...`,
+        there:    `              There you go!`,
+        playing:  `        Playing sweet, sweet music!`,
+        abit:     `            Just a bit more...`,
+        fine:     `             You're fine here,` + `\n` +
+                  `         try it on an iOS device.`,
+        decoding: `             Decoding audio... `,
+        waiting:  `        Waiting for touch unlock...`
+    };
+
+    let element = document.getElementById(id);
+    element && (element.textContent = messages[key]);
+};
+
+setMessage('message', 'tap');
+setMessage('status', 'loading');
+
+window.onblur = () =>
+{
+    if(!locked)
+    {
+        context.suspend();
+    }
+};
+
+window.onfocus = () =>
+{
+    if(!locked) {
+        context.resume();
+    }
+};
